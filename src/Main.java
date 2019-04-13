5a39a80799cfc2250e0c280f368808b5ce03e3f6
aishik_messicurry <aishikbhatta@gmail.com>
2019-04-12 21:10:03 -0700

    added helper methods, cleaned up main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,59 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        Graph g = constructGraph();
+
+        Player player = new Player("Johnny", "Elite");
+        player.setCurrentRoom(g.getNode("hall"));
+
+        Chicken chicken = new Chicken("Randy", "fat", g.getNode("hall"), player);
+        chicken.move();
+        Wompus wompus = new Wompus("Charlie", "dompus", g.getNode("closet"), player);
+        Popstar popstar = new Popstar("Shelly", "stalker", g.getNode("hall"), player);
+
+        ArrayList<Creature> creatures = new ArrayList<>();
+        creatures.add(chicken);
+        creatures.add(wompus);
+        creatures.add(popstar);
+
+        String response = "";
+        Scanner s = new Scanner(System.in);
+
+        do {
+            System.out.println("You are in the " + player.getCurrentRoom().getName());
+            System.out.println("What do you want to do? >");
+            response = s.nextLine();
+
+            String command = parseResponse(response);
+
+            executeCommand(response, command, player, g, s);
+
+            moveCharacters(creatures);
+
+            System.out.println("CCC" + wompus.getCurrentRoom().getName());
+
+        } while (!response.equals("quit"));
+
+    }
+
+    private static String parseResponse(String response) {
+        int space = response.indexOf(" ");
+        String command = response;
+        if(space != -1) command = response.substring(0, space);
+        return command;
+    }
+
+    private static void moveCharacters(ArrayList<Creature> creatures) {
+        for(Creature c: creatures){
+            c.move();
+        }
+    }
+
+    private static Graph constructGraph() {
         Graph g = new Graph();
         g.addNode("hall", "long and scary");
         g.addNode("closet", "small and stuffy");
@@ -15,75 +65,30 @@ public class Main {
         g.addItem("hall", new Item("ball", "round and bouncy"));
         g.addItem("closet", new Item("teddy bear", "soft and fluffy"));
 
-        //Graph.Node current = g.getNode("hall");
-        Player player = new Player("Johnny", "Elite");
-        player.setCurrentRoom(g.getNode("hall"));
-
-        Chicken chicken = new Chicken("Randy", "fat", g.getNode("hall"), player);
-        chicken.move(g);
-        Wompus wompus = new Wompus("Charlie", "dompus", g.getNode("closet"), player);
+        return g;
+    }
 
 
-        String response = "";
-        Scanner s = new Scanner(System.in);
+    private static void executeCommand(String response, String command, Player player, Graph g, Scanner s) {
+        Command c = null;
 
-        do {
-            System.out.println("You are in the " + player.getCurrentRoom().getName());
-            System.out.println("What do you want to do? >");
-            response = s.nextLine();
+        if (command.equals("go")) {
+            c = new GoCommand();
+        } else if (command.equals("look")) {
+            c = new LookCommand();
+        } else if (command.equals("add") && response.contains("room")) {
+            c = new AddRoomCommand();
+        }
+        else if(command.equals("take")){
+            c = new TakeCommand();
+        }
+        else if(command.equals("drop")){
+            c = new DropCommand();
+        }
+        else {
+            System.out.println("Commands: go <roomname>, look, add room <roomname> quit");
+        }
 
-            int space = response.indexOf(" ");
-            String command = response;
-            if(space != -1) command = response.substring(0, space);
-
-
-            if (command.equals("go")) {
-                String room = response.substring(3);
-                HashMap<String, Graph.Node> map = player.getCurrentRoom().getNeighbors();
-                if(map.get(room) != null) player.setCurrentRoom(g.getNode(room));
-                else System.out.println("Room invalid");
-                //current = g.getNode(room);
-            } else if (command.equals("look")) {
-                System.out.println("Current exits: " + player.getCurrentRoom().getNeighborNames());
-                System.out.println("Current items: " + player.getCurrentRoom().displayItems());
-                System.out.println(player.getCurrentRoom().getDescription());
-                System.out.print("Current Creature(s): ");
-                Graph.printCreatures(player.getCurrentRoom());
-                System.out.println();
-            } else if (command.equals("add") && response.contains("room")) {
-                String room = response.substring(9);
-                System.out.println("Description: ");
-                response = s.nextLine();
-                g.addNode(room, response);
-                g.addDirectedEdge(player.getCurrentRoom().getName(), room);
-            } else if (command.equals("quit")) continue;
-            else if(command.equals("take")){
-                String itemName = response.substring(5);
-                Item item = player.getCurrentRoom().removeItem(itemName);
-                if(item == null) System.out.println("Item not in room");
-                else {
-                    player.addItem(item);
-                    System.out.println(itemName + " has been picked up from the " + player.getCurrentRoom().getName());
-                }
-            }
-            else if(command.equals("drop")){;
-                String itemName = response.substring(5);
-                Item item = player.removeItem(itemName);
-                if(item == null) System.out.println("Item not in room");
-                else {
-                    player.getCurrentRoom().addItem(item);
-                    System.out.println(itemName + " has been dropped off at the " + player.getCurrentRoom().getName());
-                }
-            }
-            else {
-                System.out.println("Commands: go <roomname>, look, add room <roomname> quit");
-            }
-
-            chicken.move(g);
-            System.out.println("CCC" + chicken.getCurrentRoom().getName());
-            wompus.move(g);
-
-        } while (!response.equals("quit"));
-
+        if(c != null) c.execute(response, player, g, s);
     }
 }
